c1649caf580ab7cac4eac3657772ae088d336260
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 20:43:30 +1100

    Added if statement.

--- control_flow_statements/Program.cs
+++ control_flow_statements/Program.cs
@@ -11,6 +11,18 @@ namespace control_flow_statements
             Console.Write("Please enter your age: ");
             userAge = Convert.ToInt32(Console.ReadLine());
 
+            if (userAge < 0 || userAge > 100)
+            {
+                Console.WriteLine("Invalid Age");
+                Console.WriteLine("Age must be between 0 and 100");
+            }
+            else if (userAge < 21)
+                Console.WriteLine("You need parental consent");
+            else
+            {
+                Console.WriteLine("Congratulations!");
+                Console.WriteLine("You may sign up for the event!");
+            }
         }
     }
 }
