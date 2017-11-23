ea49821db8d79fb79f038244f0545f7a6f6dc0d3
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 20:42:21 +1100

    First commit

---
+++ control_flow_statements/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace control_flow_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge;
+
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
